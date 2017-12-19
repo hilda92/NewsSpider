b16155be1690344808992641535340f0d12997af
sunling <sunling6@jd.com>
2017-12-19 18:02:50 +0800

    test

--- NewsSpider/NewsSpider/spiders/SinaNews_spider.py
+++ NewsSpider/NewsSpider/spiders/SinaNews_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from NewsSpider.items import SinaNewsItem
 
 class SinaNewsSpider(scrapy.Spider):
     name = "sinaNews"
@@ -6,8 +7,11 @@ class SinaNewsSpider(scrapy.Spider):
     start_urls = ["http://med.sina.com/article_gategory_103.html"]
 
     def parse(self,response):
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-
+        # filename = response.url.split("/")[-2]
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        item = SinaNewsItem()
+        title = response.xpath("/html/head/title/text()").extract()
+        item["title"] = title[0][:-5]
+        return item
 
